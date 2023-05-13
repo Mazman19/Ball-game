9b96e790688d15bab27cf695b0988c5bb62c95b6
Mazman19 <mazmangrigoryan56@gmail.com>
2023-05-13 14:42:17 +0300

    ball init

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+const player1Input = document.querySelector('#player1');
+const player2Input = document.querySelector('#player2');
+const startBtn = document.querySelector('#start');
+
+startBtn,addEventListener('click', function() {
+    const player1Name = player1Input.value;
+    const player2Name = player2Input.value;
+
+    if (player1Name && player2Name) {
+        start();
+    } else {
+        alert('please feel inputs')
+    }
+});
